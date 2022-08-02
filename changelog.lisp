85bb346670be49e8375391e0d51a7b693d9cf4f3
Alexander Artemenko <svetlyak.40wt@gmail.com>
2022-08-02 12:26:49 +0300

    v1.3.1-pre6

--- changelog.lisp
+++ changelog.lisp
@@ -7,7 +7,7 @@
 
 (defchangelog ()
   (1.3.1 2022-08-01
-         "Don't update docs if there is no changes since last documentation update. 3")
+         "Don't update docs if there is no changes since last documentation update. 4")
   (1.3.0 2021-10-19
          "Don't upload docs when action is used on pull-request.")
   (1.2.0 2021-05-10
